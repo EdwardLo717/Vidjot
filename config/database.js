1e8101025973a4fec3265ca7a7290b64817af27e
Edward Lo <edwardlo717@gmail.com>
2019-04-29 15:26:25 +0800

    change the dbbase

--- config/database.js
+++ config/database.js
@@ -1,5 +1,6 @@
-//if (process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI:'mongodb+srv://edward:Loh1218/@cluster0-imetn.mongodb.net/vidjot-prod?retryWrites=true'}
-//} else {
-//  module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
-//}
+if (process.env.NODE_ENV === 'production'){
+  module.exports = {mongoURI:'mongodb://edwardlo:Loh1218@ds131971.mlab.com:31971/heroku_lfhhd830'}
+} else {
+  module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
+  //module.exports = {mongoURI:'mongodb://edwardlo:Loh1218@ds131971.mlab.com:31971/heroku_lfhhd830'}
+}
