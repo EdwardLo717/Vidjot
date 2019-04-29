e7ae53bc82af41aad79fccb5ca2199a011f885e2
Edward Lo <edwardlo717@gmail.com>
2019-04-29 13:36:50 +0800

    remove local mongodb

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
-if (process.env.NODE_ENV === 'production'){
+//if (process.env.NODE_ENV === 'production'){
   module.exports = {mongoURI:'mongodb+srv://edward:Loh1218@cluster0-imetn.mongodb.net/vidjot-prod?retryWrites=true'}
-} else {
-  module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
-}
+//} else {
+//  module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
+//}
