6858e34d314bef9916bb8a8425603271c4229c9c
Edward Lo <edwardlo717@gmail.com>
2019-04-29 15:34:05 +0800

    change the dbbase

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI:'mongodb://edwardlo:Loh1218@ds131971.mlab.com:31971/heroku_lfhhd830'}
+  module.exports = {mongoURI:'mongodb://edward:city1218@ds131971.mlab.com:31971/heroku_lfhhd830'}                         
 } else {
-  module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
-  //module.exports = {mongoURI:'mongodb://edwardlo:Loh1218@ds131971.mlab.com:31971/heroku_lfhhd830'}
+  //module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
+  module.exports = {mongoURI:'mongodb://edward:city1218@ds131971.mlab.com:31971/heroku_lfhhd830'}
 }
