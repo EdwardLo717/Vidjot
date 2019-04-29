e76baf15d55796085f28b74a1e5db3d108e0a06b
Edward Lo <edwardlo717@gmail.com>
2019-04-29 14:03:46 +0800

    modify the database connecting string

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 //if (process.env.NODE_ENV === 'production'){
-  module.exports = {mongoURI:'mongodb+srv://edward:Loh1218@cluster0-imetn.mongodb.net/vidjot-prod?retryWrites=true'}
+  module.exports = {mongoURI:'mongodb+srv://edward:<password>@cluster0-imetn.mongodb.net/test?retryWrites=true'}
 //} else {
 //  module.exports = {mongoURI:'mongodb://localhost/vidjot-dev'}; 
 //}
